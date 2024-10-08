1518584b6935b30235ebadbdaa67f8382378e7a2
Toms Zacharia <tomszacharia@Tomss-MacBook-Pro.local>
2024-10-07 23:10:15 -0400

    Added 2 scripts. #1 to export services as csv. #2 to convert teams to systems (on Flexport's request)

---
+++ scripts/export_services_as_csv/export_services_as_csv.py
@@ -0,0 +1,69 @@
+import requests
+import pandas as pd
+
+# Set your OpsLevel API endpoint and access token
+API_TOKEN = '<your API token>'
+url = 'https://api.opslevel.com/graphql'
+headers = {
+    'Authorization':  f"Bearer {API_TOKEN}",
+    'Content-Type': 'application/json',
+    'Graphql-Visibility': 'internal'
+}
+
+# GraphQL query to fetch services
+query = """
+query getServices($first: Int, $after: String) {
+  account {
+    allServices(first: $first, after: $after, sortBy: name_ASC) {
+        pageInfo {
+            endCursor
+            hasNextPage
+        }
+        nodes {
+            id
+            name
+            timestamps {
+                updatedAt
+            }        
+        }
+    }
+  }
+}
+"""
+def fetch_services():
+    services = []
+    has_next_page = True
+    after = None
+
+    while has_next_page:
+        variables = {
+            "first": 100,  # Number of items to fetch per request
+            "after": after
+        }
+
+        response = requests.post(url, headers=headers, json={'query': query, 'variables': variables})
+        print(response.json())
+        if response.status_code == 200:
+            data = response.json()['data']['account']['allServices']
+            services.extend(data['nodes'])
+            has_next_page = data['pageInfo']['hasNextPage']
+            after = data['pageInfo']['endCursor']
+        else:
+            print(f"Error: {response.status_code}, {response.text}")
+            break
+
+    return services
+
+# Fetch the services
+services = fetch_services()
+
+# Extract id and name
+service_data = [(service['id'], service['name'], service['timestamps']['updatedAt']) for service in services]
+
+# Create a DataFrame
+df = pd.DataFrame(service_data, columns=['id', 'name', 'updatedAt'])
+
+# Save to CSV
+csv_path = 'services_graphqlapi.csv'
+df.to_csv(csv_path, index=False)
+print(f"CSV file has been created at {csv_path}")
